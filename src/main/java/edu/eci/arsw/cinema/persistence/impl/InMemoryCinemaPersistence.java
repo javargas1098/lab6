cadbfb6132f6e06ea1dcb171882c9272b3811088
javargas1098 <ja.vargas1098@gmail.com>
2019-02-28 10:01:57 -0500

    primero

---
+++ src/main/java/edu/eci/arsw/cinema/persistence/impl/InMemoryCinemaPersistence.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.cinema.persistence.impl;
+
+import edu.eci.arsw.cinema.model.Cinema;
+import edu.eci.arsw.cinema.model.CinemaFunction;
+import edu.eci.arsw.cinema.model.Movie;
+import edu.eci.arsw.cinema.persistence.CinemaException;
+import edu.eci.arsw.cinema.persistence.CinemaPersistenceException;
+import edu.eci.arsw.cinema.persistence.CinemaPersitence;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.hibernate.validator.internal.util.privilegedactions.GetMethodFromPropertyName;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author cristian
+ */
+@Component("cps")
+public class InMemoryCinemaPersistence implements CinemaPersitence {
+
+	private final Map<String, Cinema> cinemas = new HashMap<>();
+
+	public InMemoryCinemaPersistence() {
+		// load stub data
+		String functionDate = "2018-12-18 15:30";
+		List<CinemaFunction> functions = new ArrayList<>();
+		CinemaFunction funct1 = new CinemaFunction(new Movie("SuperHeroes Movie", "Action"), functionDate);
+		CinemaFunction funct2 = new CinemaFunction(new Movie("The Night", "Horror"), functionDate);
+		functions.add(funct1);
+		functions.add(funct2);
+		Cinema c = new Cinema("cinemaX", functions);
+		cinemas.put("cinemaX", c);
+		
+		String functionDate2 = "2018-12-18 15:30";
+		List<CinemaFunction> functions2 = new ArrayList<>();
+		CinemaFunction funct3 = new CinemaFunction(new Movie("SuperHeroes Movie", "Action"), functionDate2);
+		CinemaFunction funct4 = new CinemaFunction(new Movie("The Night", "Horror"), functionDate2);
+		functions2.add(funct3);
+		functions2.add(funct4);
+		Cinema c1 = new Cinema("cinemaY", functions2);
+		cinemas.put("cinemaY", c1);
+	}
+
+	@Override
+	public boolean buyTicket(int row, int col, String cinema, String date, String movieName) throws CinemaException {
+		Cinema cine = cinemas.get(cinema);
+		for (CinemaFunction funtion : cine.getFunctions()) {
+
+			if (funtion.getMovie().getName().equals(movieName) && funtion.getDate().equals(date)) {
+
+				funtion.buyTicket(row, col);
+				return true;
+
+			}
+
+		}
+		return false;
+
+	}
+
+	@Override
+	public List<CinemaFunction> getFunctionsbyCinemaAndDate(String cinema, String date) {
+		Cinema cine = cinemas.get(cinema);
+		List<CinemaFunction> fun = new ArrayList<>();
+		for (CinemaFunction funtion : cine.getFunctions()) {
+			if (funtion.getDate().equals(date)) {
+				fun.add(funtion);
+
+			}
+		}
+		return fun;
+
+	}
+	@Override
+	public List<CinemaFunction> getFunctionsbyCinemaAndDateAndHour(String cinema, String date,String nombre) {
+		Cinema cine = cinemas.get(cinema);
+		List<CinemaFunction> fun = new ArrayList<>();
+		for (CinemaFunction funtion : cine.getFunctions()) {
+			if (funtion.getDate().equals(date.split(" ")[0]) && funtion.getHour().equals(date.split(" ")[1]) && funtion.getMovie().getName().equals(nombre)) {
+				fun.add(funtion);
+
+			}
+		}
+		return fun;
+
+	}
+
+	@Override
+	public void saveCinema(Cinema c) throws CinemaPersistenceException {
+		if (cinemas.containsKey(c.getName())) {
+			throw new CinemaPersistenceException("The given cinema already exists: " + c.getName());
+		} else {
+			cinemas.put(c.getName(), c);
+		}
+	}
+
+	@Override
+	public Cinema getCinema(String name) throws CinemaPersistenceException {
+		return cinemas.get(name);
+	}
+
+	@Override
+	public Set<Cinema> getAllCinemas() throws CinemaPersistenceException {
+		// TODO Auto-generated method stub
+		Set<Cinema> allCinemas = new HashSet<Cinema>();
+		for (Map.Entry<String, Cinema> entry : cinemas.entrySet()) {
+			allCinemas.add(entry.getValue());
+		}
+		return allCinemas;
+	}
+
+
+
+}
